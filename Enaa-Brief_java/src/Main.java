67c69639f89041d6d4700ae56bfe4537b4d365ee
KHALID <khalidboumaghdar07@gmail.com>
2024-12-15 16:42:47 +0100

    déclaration variable et import package Scanner pour saisez

---
+++ Enaa-Brief_java/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+
+        Scanner scanner = new Scanner(System.in);
+        double x;
+        double y;
+        int choix;
+        
+
+
+    }
+
+}
