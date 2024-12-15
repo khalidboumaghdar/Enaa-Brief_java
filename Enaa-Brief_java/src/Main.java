1828d224defc60e27685f0278c8365e39321a364
KHALID <khalidboumaghdar07@gmail.com>
2024-12-15 17:05:39 +0100

    create menu et aficher

--- Enaa-Brief_java/src/Main.java
+++ Enaa-Brief_java/src/Main.java
@@ -3,6 +3,19 @@ import java.lang.Math;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+	static void menu(){
+       System.out.println("\n \t Welcome to the Calculator Program !!");
+       System.out.println("1:Addition  ");
+       System.out.println("2:Soustraction  ");
+       System.out.println("3:Multiplication  ");
+       System.out.println("4:Division  ");
+       System.out.println("5:Puissance  ");
+       System.out.println("6:Racine carrée  ");
+       System.out.println("7:Factorielle");
+       System.out.println("8:Au revoir !!");
+       System.out.println("Veullez enter operation :");
+   }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -12,7 +25,7 @@ public class Main {
         double x;
         double y;
         int choix;
-        
+        menu();
 
 
     }
